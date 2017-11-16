3aaa1fd26cd5b56245ce4aa15544a1e79e1b46c5
Lukas Kern <lkern.mmt-b2016@fh-salzburg.ac.at>
2017-11-16 11:58:46 +0100

    mina

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,9 +2,9 @@ require 'mina/deploy'
 require 'mina/git'
 
 set :application_name, 'cms'
-set :domain, '46.101.146.201'
+set :domain, '165.227.146.118'
 set :deploy_to, '/var/www/html'
-set :repository, 'https://github.com/designrubenz/cms.git'
+set :repository, 'https://github.com/stefanelsat/cms'
 set :branch, 'master'
 set :user, 'cms'
 
