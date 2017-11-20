b9487118310691abad3a0bb76efca915084f4168
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2017-11-20 19:51:24 +0100

    Update responsive design add menu WIP

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -1,6 +1,20 @@
 console.log('I go first!')
 
 const burgerIcon = document.getElementById('burger-menu')
+const menu = document.getElementsByTagName('nav')[0]
+
 burgerIcon.addEventListener('click', function() {
-  console.log(this.children)
+  for(let child of this.children) {
+    if(!child.classList.contains('change')) {
+      child.classList += ' change'
+      menu.style.display = 'block'
+    }
+    else {
+      child.classList.remove('change')
+      menu.style.disply = 'none'
+      console.log(menu.style.display)
+    }
+
+  }
 })
+
