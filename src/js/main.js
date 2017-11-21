141bff625293ab9880ca2e061d9feaaac982a09e
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2017-11-21 20:01:36 +0100

    Fix resizing bug

--- src/js/main.js
+++ src/js/main.js
@@ -5,36 +5,51 @@ const nav = document.getElementsByTagName('nav')[0]
 const menu = document.getElementsByTagName('ul')[0]
 const links = document.getElementsByClassName('link')
 
+
+// reset the burger menu when resizing at width > 640
+
+window.addEventListener('resize', function () {
+  if(window.innerWidth > 640)
+    menu.style.opacity = '1'
+  else
+    menu.style.opacity = '0'
+
+  for(let link of links) {
+    link.removeEventListener('click')
+  }
+})
+
 burgerIcon.addEventListener('click', function() {
+  addEventCloseMenu()
+
   for(let child of this.children) {
     if(!child.classList.contains('change')) {
       child.classList += ' change'
-      //menu.style.display = 'inline-block'
       menu.style.opacity = '1'
       nav.style.height = '100vh'
-
     }
     else {
       child.classList.remove('change')
-      //menu.style.display = 'none'
       menu.style.opacity = '0'
       nav.style.height = '41px'
-
     }
 
   }
 })
 
-for(let link of links) {
-  link.addEventListener('click', function() {
-    for(let child of burgerIcon.children) {
-      child.classList.remove('change')
-    }
-
-    menu.style.opacity = '0'
-    nav.style.height = '41px'
-  })
+function addEventCloseMenu() {
+  for(let link of links) {
+    link.addEventListener('click', function() {
+      for(let child of burgerIcon.children) {
+        child.classList.remove('change')
+      }
+      menu.style.opacity = '0'
+      nav.style.height = '41px'
+    })
+  }
 }
+
+
 
 
 
