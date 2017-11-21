9bdc376f351810c38ce1a48cdbb1e428358f3563
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2017-11-21 14:13:54 +0100

    Update responsive design

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -1,6 +1,7 @@
 console.log('I go first!')
 
 const burgerIcon = document.getElementById('burger-menu')
+const nav = document.getElementsByTagName('nav')[0]
 const menu = document.getElementsByTagName('ul')[0]
 
 burgerIcon.addEventListener('click', function() {
@@ -8,11 +9,12 @@ burgerIcon.addEventListener('click', function() {
     if(!child.classList.contains('change')) {
       child.classList += ' change'
       menu.style.display = 'inline-block'
-
+      nav.style.maxHeight = '550px'
     }
     else {
       child.classList.remove('change')
       menu.style.display = 'none'
+      nav.style.maxHeight = '50px'
     }
 
   }
