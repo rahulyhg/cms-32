12de6044542530fb68ee108bad1342a3ce363ec2
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2017-11-21 19:01:52 +0100

    Update responsive design fixed css transition bug

--- src/js/main.js
+++ src/js/main.js
@@ -3,22 +3,39 @@ console.log('I go first!')
 const burgerIcon = document.getElementById('burger-menu')
 const nav = document.getElementsByTagName('nav')[0]
 const menu = document.getElementsByTagName('ul')[0]
+const links = document.getElementsByClassName('link')
 
 burgerIcon.addEventListener('click', function() {
   for(let child of this.children) {
     if(!child.classList.contains('change')) {
       child.classList += ' change'
-      menu.style.display = 'inline-block'
-      nav.style.maxHeight = '550px'
+      //menu.style.display = 'inline-block'
+      menu.style.opacity = '1'
+      nav.style.height = '100vh'
+
     }
     else {
       child.classList.remove('change')
-      menu.style.display = 'none'
-      nav.style.maxHeight = '50px'
+      //menu.style.display = 'none'
+      menu.style.opacity = '0'
+      nav.style.height = '41px'
+
     }
 
   }
 })
+
+for(let link of links) {
+  link.addEventListener('click', function() {
+    for(let child of burgerIcon.children) {
+      child.classList.remove('change')
+    }
+
+    menu.style.opacity = '0'
+    nav.style.height = '41px'
+  })
+}
+
 
 
 (function report_ready(){console.log('ready!')})()
