9f8248de5dd1d51adf9cd06be4ce00f3563fc987
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2017-11-21 14:17:57 +0100

    Add main js workaround

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+console.log('I go first!')
+
+const burgerIcon = document.getElementById('burger-menu')
+const nav = document.getElementsByTagName('nav')[0]
+const menu = document.getElementsByTagName('ul')[0]
+
+burgerIcon.addEventListener('click', function() {
+  for(let child of this.children) {
+    if(!child.classList.contains('change')) {
+      child.classList += ' change'
+      menu.style.display = 'inline-block'
+      nav.style.maxHeight = '550px'
+    }
+    else {
+      child.classList.remove('change')
+      menu.style.display = 'none'
+      nav.style.maxHeight = '50px'
+    }
+
+  }
+})
+
+
+(function report_ready(){console.log('ready!')})()
