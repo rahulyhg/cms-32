91d735a00f38fd3da960fe35ede458a1bae2954f
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2017-11-27 12:13:59 +0100

    Update index

---
+++ wordpress/wp-content/themes/screendesign-alex_sumner/footer.php
@@ -0,0 +1,21 @@
+<footer>
+  <h3>
+    &copy; <a title="Senden Sie mir eine Kontaktanfrage"
+              class="special"
+              href="mailto:alex@sumner-web.at">Alex Sumner</a>, Webdesigner zu Salzburg
+  </h3>
+</footer>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID -->
+<script src="assets/js/main.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments) },i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-XXXXXXXXX-X', 'auto');
+  ga('send', 'pageview');
+</script>
+</body>
+</html>
---
+++ wordpress/wp-content/themes/screendesign-alex_sumner/header.php
@@ -0,0 +1,59 @@
+<!doctype html>
+
+<!--
+TODO:
+* change apple-mobile-web-app-title
+* add Google-Analytics ID
+* create new images/icon-touch
+
+-->
+
+<html lang="de">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="My name is Alex Sumner and I am a Web designer.">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Webdesign by Alex Sumner</title>
+
+  <!-- Add to homescreen for Chrome on Android -->
+  <meta name="mobile-web-app-capable" content="yes">
+  <link rel="icon" sizes="192x192" href="images/icons/design.png">
+
+  <!-- Add to homescreen for Safari on iOS -->
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="CHANGE_THIS_TO_SITE_NAME"> <!-- change this -->
+  <link rel="apple-touch-icon" href="images/icons-touch/apple-touch-icon.png">
+
+  <!-- Tile icon for Win8 (144x144 + tile color) -->
+  <meta name="msapplication-TileImage" content="images/icons-touch/ms-touch-icon-144x144-precomposed.png">
+  <meta name="msapplication-TileColor" content="#3372DF">
+
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <!-- SEO: If your mobile URL is different from the desktop URL, add a canonical link to the desktop page https://developers.google.com/webmasters/smartphone-sites/feature-phones -->
+  <!--
+  <link rel="canonical" href="http://www.example.com/">
+  -->
+
+</head>
+<body>
+<!-- Add your site content here -->
+<header>
+  <h1><a class="normal" href="#">Alex Sumner</a></h1>
+  <nav>
+    <a id="burger-menu">
+      <div class="row1"></div>
+      <div class="row2"></div>
+      <div class="row3"></div>
+    </a>
+    <ul>
+      <li><a class="link" href="#">Start</a></li>
+      <li><a class="link" href="#leistungen">Leistungen</a></li>
+      <li><a class="link" href="#news">Blog</a></li>
+      <li><a class="link" href="#referenzen">Über mich</a></li>
+      <li><a class="link" href="#contact">Kontakt</a></li>
+    </ul>
+  </nav>
+</header>
+
